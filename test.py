45ac435f55c59f53e298fb9354a431362d0bf89b
Miracle Apata <prmpsmart@gmail.com>
2024-04-09 16:25:09 +0100

    updated test.py

--- test.py
+++ test.py
@@ -7,15 +7,18 @@ url = "http://127.0.0.1:8000"
 email = "miracle.apata@mailinator.com"
 password = "securepassword123"
 
+email = "testuser@mailinator.com"
+password = "password"
+
 auth = AuthTest(url)
 
-# auth.register(
-#     full_name="Miracle Apata",
-#     email=email,
-#     phone_number="+2348168524477",
-#     password=password,
-#     # image='',
-# )
+auth.register(
+    full_name="Test User",
+    email=email,
+    phone_number="+23467269392",
+    password=password,
+    # image='',
+)
 
 auth.login(
     email=email,
@@ -29,22 +32,24 @@ auth.login(
 pipeline = PipelineTest(url)
 pipeline.access_token = auth.access_token
 
-pipelines = ["P1", "P2", "P3"]
+pipelines = ["Personal", "Business"]
 pi = 0
 
 # pipeline.get_pipelines()
+# exit()
+
 
 # pipelines
 # pipeline.create_pipeline(pipelines[pi+1], 5)
 pipeline.get_pipeline(pipelines[pi])
 # pipeline.update_pipeline(pipelines[pi], 900)
-pipeline.delete_pipeline(pipelines[pi])
+# pipeline.delete_pipeline(pipelines[pi])
 
 
 branches = ["B1", "B2", "B3"]
 bi = 0
 # branches
-pipeline.create_branch(pipelines[pi], branches[bi], 40)
-# pipeline.get_branch(pipelines[pi], branches[bi])
+# pipeline.create_branch(pipelines[pi], branches[bi], 40)
+pipeline.get_branch(pipelines[pi], branches[bi])
 # pipeline.update_branch(pipelines[pi], branches[bi], 30)
 # pipeline.delete_branch(pipelines[pi], branches[bi])
